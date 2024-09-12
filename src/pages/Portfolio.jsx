ce0081a3b3cf0eb897209f9bce73bd3fff333918
Sean <Seanecrocker@gmail.com>
2024-09-12 12:48:06 -0500

    adding images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Project from '../components/Project';
+import chargingStationImage from '../assets/ChargingStations.png';
+import textBasedGame from '../assets/TextBasedGame.png'
+import searchBookEngine from '../assets/SearchBookEngine.png'
 
 const Portfolio = () => (
   <section className="portfolio">
@@ -10,7 +13,7 @@ const Portfolio = () => (
         description="Project one project where a group of collaborators needed to come up with a real-world problem 
         that could be solved with code. The problem being solved in this project is a way for electric car owners to quickly find a 
         charging station to charge their car. This project included, javascript, html, css, tomtom api, and opan charge api." 
-        imageUrl="/images/project1.png" 
+        imageUrl={chargingStationImage}
         projectUrl="https://mackemo.github.io/project-1-charge-car/"
         repoUrl="https://github.com/mackemo/project-1-charge-car"
       />
@@ -19,7 +22,7 @@ const Portfolio = () => (
         description="Project two is a MERN stack single-page application allowing users to signup and login to save user progress. On the dashboard, 
         a quick description of the game is provided and a button to start the game. For each prompt of the game, users are given two 
         choices to take action that leads the user down a path until an end result is met." 
-        imageUrl="/images/project2.png" 
+        imageUrl={textBasedGame}
         projectUrl="https://text-based-adventure-game.onrender.com/"
         repoUrl="https://github.com/Flem-House-Dev/Text-Based-Adventure-Game"
       />
@@ -27,7 +30,7 @@ const Portfolio = () => (
         title="Project Three" 
         description="Project 3 is a Book Search Engine which is a MERN (MongoDB, Express.js, React, Node.js) stack application that allows users to search for books using 
         the Google Books API and find out more about their favorite books!" 
-        imageUrl="/images/project3.png" 
+        imageUrl={searchBookEngine} 
         projectUrl="https://book-search-engine-20-aq6h.onrender.com/"
         repoUrl="https://github.com/Seanecrocker/Book-Search-Engine"
       />
